401ccebda0056bbbf4c3824cd4dd54e601d4aab3
cemil ilkim teke <cemililkimteke5934@gmail.com>
2025-07-23 15:44:45 +0300

    Add DiffChecker Page

---
+++ DevTools/Controllers/DiffCheckerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevTools.Controllers
+{
+    public class DiffCheckerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
