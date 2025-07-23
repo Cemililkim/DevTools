466d7e2ec7e5ef60dfe0a83b31b43072f5bb569f
cemil ilkim teke <cemililkimteke5934@gmail.com>
2025-07-23 10:27:13 +0300

    Add project files.

---
+++ DevTools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DevTools.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
