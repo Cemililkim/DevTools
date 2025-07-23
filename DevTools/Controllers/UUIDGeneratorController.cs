43eb78dc15be6b8b9ba45508e627599118368e5a
cemil ilkim teke <cemililkimteke5934@gmail.com>
2025-07-23 14:45:57 +0300

    Add UUID Page

---
+++ DevTools/Controllers/UUIDGeneratorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevTools.Controllers
+{
+    public class UUIDGeneratorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
