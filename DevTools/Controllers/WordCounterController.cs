84f6a69302aa3a150368dcea89e953af0421fc98
cemil ilkim teke <cemililkimteke5934@gmail.com>
2025-07-24 09:06:17 +0300

    Add CaseConverter, DuplicateLineRemover and WordCounter Pages

---
+++ DevTools/Controllers/WordCounterController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevTools.Controllers
+{
+    public class WordCounterController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
