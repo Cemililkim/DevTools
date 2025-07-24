1fd96fcc4c5cf34247ad51bc14e0d1d22b989f6f
cemil ilkim teke <cemililkimteke5934@gmail.com>
2025-07-24 10:37:47 +0300

    Add QRCodeTools Page

---
+++ DevTools/Controllers/QRCodeToolsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevTools.Controllers
+{
+    public class QRCodeToolsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
